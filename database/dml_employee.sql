13e45f2b8f9d150f04e4aa07147db4012b00c603
andriganteng <42957537+andriganteng@users.noreply.github.com>
2018-09-04 15:35:20 +0700

    dummy data

---
+++ database/dml_employee.sql
@@ -0,0 +1,11 @@
+INSERT INTO Employee (email, pass, address, nama, username, notel)
+VALUES ('admiralbulldog@gmail.com', 'theinternational', 'jl.sweeden no.12', 'henrik kjelberg', 'admiralbulldog', '08123456789');
+
+INSERT INTO Employee (email, pass, address, nama, username, notel)
+VALUES ('krisnadibima@gmail.com', 'leaderoftheyear', 'jl.telekomunikasi no.67', 'krisnadi bima', 'admiralnelson', '08987654321');
+
+INSERT INTO Employee (email, pass, address, nama, username, notel)
+VALUES ('andrisonjaya@gmail.com', 'uzumakiandri', 'jl.mekar wangi no.32', 'andri amirul', 'andriganteng', '081223212556');
+
+INSERT INTO Employee (email, pass, address, nama, username, notel)
+VALUES ('rizaldolaksana@gmail.com', 'gamersoftheyear', 'jl.terusan telekomunikasi no.60', 'rizaldo laksana', 'crystaline', '082113340227');
