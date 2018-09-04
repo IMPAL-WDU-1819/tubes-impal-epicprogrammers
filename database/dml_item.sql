f7ea08e03f4d1391956b0e62f229a880c2b5cf8b
andriganteng <42957537+andriganteng@users.noreply.github.com>
2018-09-04 16:32:17 +0700

    dummy data

---
+++ database/dml_item.sql
@@ -0,0 +1,11 @@
+INSERT INTO Item (itemNo, namaMobil, harga, tipeNo, warna, jobId)
+VALUES (1, 'honda all new city', 100000000, 0004, 'merah', 10001);
+
+INSERT INTO Item (itemNo, namaMobil, harga, tipeNo, warna, jobId)
+VALUES (2, 'mitsubishi xpander', 150000000, 0003, 'putih', 20002);
+
+INSERT INTO Item (itemNo, namaMobil, harga, tipeNo, warna, jobId)
+VALUES (3, 'nissan GT-R', 200000000, 0002, 'hitam', 30003);
+
+INSERT INTO Item (itemNo, namaMobil, harga, tipeNo, warna, jobId)
+VALUES (4, 'bmw 4-series convertible', 180000000, 0001, 'silver', 40004);
