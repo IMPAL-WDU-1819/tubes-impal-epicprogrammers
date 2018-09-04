22e99455a1a89ebf172ce320d56a9288e9bb276c
andriganteng <42957537+andriganteng@users.noreply.github.com>
2018-09-04 16:26:08 +0700

    dummy data

--- database/dml_job.sql
+++ database/dml_job.sql
@@ -2,10 +2,10 @@ INSERT INTO Job (jobid, progress, employeeEmail)
 VALUES (10001, 'memasang rem mobil', 'rizaldoaksana@gmail.com');
 
 INSERT INTO Job (jobid, progress, employeeEmail)
-VALUES (10002, 'memberi warna pada mobil', 'andrisonjaya@gmail.com');
+VALUES (20002, 'memberi warna pada mobil', 'andrisonjaya@gmail.com');
 
 INSERT INTO Job (jobid, progress, employeeEmail)
-VALUES (10003, 'memasang sparepart', 'krisnadibima@gmail.com');
+VALUES (30003, 'memasang sparepart', 'krisnadibima@gmail.com');
 
 INSERT INTO Job (jobid, progress, employeeEmail)
-VALUES (10004, 'memasang kursi mobil', 'admiralbulldog@gmail.com');
+VALUES (40004, 'memasang kursi mobil', 'admiralbulldog@gmail.com');
