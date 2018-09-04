45d348b204e64285b7e53388caae602f796fb930
andriganteng <42957537+andriganteng@users.noreply.github.com>
2018-09-04 15:15:03 +0700

    Delete dml_consumer.sql

--- database/dml_consumer.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO Consumer (Email string, username string, notel string, address string, nama string, pass string)
-VALUES ('wahyu@gmail.com', 'wahyuganteng', '08122151234', 'jl. telekomunikasi no.14', 'wahyu junaedi', '123456');
-
-INSERT INTO Consumer (Email string, username string, notel string, address string, nama string, pass string)
-VALUES ('laksamana@gmail.com', 'laksamanamaeda', '08321181224', 'jl. bale endah no.32', 'laksamana maeda', 'laksamanaganteng');
-
-INSERT INTO Consumer (Email string, username string, notel string, address string, nama string, pass string)
-VALUES ('nurul@gmail.com', 'djaisah', '08202050204', 'jl. sukabirus no.9', 'nurul sulul', 'akucintadia');
