9589ad24206a48c50f4358619fab919e0dac9bb5
andriganteng <42957537+andriganteng@users.noreply.github.com>
2018-09-04 15:54:10 +0700

    dummy data

---
+++ database/dml_job.sql
@@ -0,0 +1,11 @@
+INSERT INTO Job (jobid, progress, employeeEmail)
+VALUES ('F0001', 'memasang rem mobil', 'rizaldoaksana@gmail.com');
+
+INSERT INTO Job (jobid, progress, employeeEmail)
+VALUES ('F0002', 'memberi warna pada mobil', 'andrisonjaya@gmail.com');
+
+INSERT INTO Job (jobid, progress, employeeEmail)
+VALUES ('F0003', 'memasang sparepart', 'krisnadibima@gmail.com');
+
+INSERT INTO Job (jobid, progress, employeeEmail)
+VALUES ('F0004', 'memasang kursi mobil', 'admiralbulldog@gmail.com');
