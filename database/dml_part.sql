a2d00bec380dde53e467cf29522a364141cee392
Andri Amirul Sonjaya <42957537+andriganteng@users.noreply.github.com>
2018-09-04 16:52:02 +0700

    dummy data

---
+++ database/dml_part.sql
@@ -0,0 +1,11 @@
+INSERT INTO Part (itemNo, namaPart, harga, qty)
+VALUES (1, 'timing belt', 300000, 8);
+
+INSERT INTO Part (itemNo, namaPart, harga, qty)
+VALUES (2, 'perseneling', 20000000, 4);
+
+INSERT INTO Part (itemNo, namaPart, harga, qty)
+VALUES (3, 'bodi mobil', 2000000, 4);
+
+INSERT INTO Part (itemNo, namaPart, harga, qty)
+VALUES (4, 'air conditioning', 2000000, 4);
