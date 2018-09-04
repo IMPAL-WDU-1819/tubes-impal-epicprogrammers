bfb685fcfea35667312131eca71dd22eca05d5b6
Andri Amirul Sonjaya <42957537+andriganteng@users.noreply.github.com>
2018-09-04 16:57:07 +0700

    dummy data

---
+++ database/dml_part2item.sql
@@ -0,0 +1,11 @@
+INSERT INTO Part2Item (itemNoItem, itemNoPart)
+VALUES (1, 1001);
+
+INSERT INTO Part2Item (itemNoItem, itemNoPart)
+VALUES (2, 1002);
+
+INSERT INTO Part2Item (itemNoItem, itemNoPart)
+VALUES (3, 1003);
+
+INSERT INTO Part2Item (itemNoItem, itemNoPart)
+VALUES (4, 1004);
