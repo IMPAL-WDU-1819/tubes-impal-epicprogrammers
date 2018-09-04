cb3c4b93170bbc98b9343d2788291f7b4de7898e
Andri Amirul Sonjaya <42957537+andriganteng@users.noreply.github.com>
2018-09-04 17:07:17 +0700

    dummy data

---
+++ database/dml_user2item.sql
@@ -0,0 +1,11 @@
+INSERT INTO User (emailuser, itemNo)
+VALUES ('kiana@gmail.com', 1);
+
+INSERT INTO User (emailuser, itemNo)
+VALUES ('mei@gmail.com', 2);
+
+INSERT INTO User (emailuser, itemNo)
+VALUES ('watashiwabukanwibudesu@gmail.com', 3);
+
+INSERT INTO User (emailuser, itemNo)
+VALUES ('jehanmahsan@gmail.com', 4);
